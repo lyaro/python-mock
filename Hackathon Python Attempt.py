6b093debee285bb8eb22d0be0feae480f6197a76
layla <144674186+lyaro@users.noreply.github.com>
2023-10-26 17:20:57 +0100

    Add files via upload

---
+++ Hackathon Python Attempt.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+def logIn():
+
+    email = input(print("Please enter your account email:  "))
+    
+    password = input(print("Please enter your account password:  "))
+    
+    print("Your email is", email, "and your password is", password)
+
+def signUp():
+    print("Welcome to Hackathon.com, please enter your details to create an account with us")
+    firstName = input(print("Enter your first name:  "))
+    lastName = input(print("Enter your last name:  "))
+    email = input(print("Enter your personal email:  "))
+    password = input(print("Enter a strong password:  "))
+
+def hackathonApply():
+    print("Thank you for optiong to apply to the up and coming hackathon")
+
+
+
